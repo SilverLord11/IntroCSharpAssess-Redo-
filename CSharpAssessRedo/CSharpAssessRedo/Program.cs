77989a063c866ef87be4811622149e9ec41814be
Logan Holt <WolfGuardLegion11@gmail.com>
2020-10-20 22:44:41 -0700

    The True Restart

---
+++ CSharpAssessRedo/CSharpAssessRedo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpAssessRedo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
